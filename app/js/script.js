672fb86f25ff8a7ec6bceea8d4b8adbe337332dd
Roland Jethro Suyom <rolandjethrosuyom@yahoo.com>
2023-01-14 19:03:51 +0300

    worked on toggle menu and overlay animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,28 @@
 const navToggle = document.getElementById("nav__toggle");
 const navbar = document.querySelector(".nav");
+const navLinks = document.querySelector(".nav__links");
+const navLink = document.querySelectorAll(".nav__link");
 const overlay = document.querySelector("#overlay");
 
+/** auto hide toggle menu and overlay when the user resize the window */
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 500) {
+        navbar.classList.remove("nav--open-menu");
+        overlay.classList.remove("overlay");
+    }
+})
+// hide and show toggle menu
+const toggleMenu = () => {
+    overlay.classList.toggle("overlay");
+    navbar.classList.toggle("nav--open-menu");
+}
 
+// hide and show toggle menu using the hamburger menu
 navToggle.addEventListener("click", () => {
-
     toggleMenu();
 })
 
-
-const toggleMenu = () => {
-    console.log("navToggle clicked");
-    navbar.classList.toggle("nav--open-menu");
-    overlay.classList.toggle("overlay");
-}
+// hide and show toggle menu using the links
+navLink.forEach(link => {
+    link.addEventListener("click", toggleMenu);
+});
