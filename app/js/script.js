51d31ec2ec103b3e3802617e970269a675a6889d
Roland Jethro Suyom <rolandjethrosuyom@yahoo.com>
2023-01-15 06:27:12 +0300

    working nav menu active state and added buttons styling

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,4 @@
+const header = document.querySelector("#header");
 const navToggle = document.getElementById("nav__toggle");
 const navbar = document.querySelector(".nav");
 const navLinks = document.querySelector(".nav__links");
@@ -24,5 +25,37 @@ navToggle.addEventListener("click", () => {
 
 // hide and show toggle menu using the links
 navLink.forEach(link => {
-    link.addEventListener("click", toggleMenu);
+    link.addEventListener("click", () => {
+        const index = [...navLink].indexOf(link);
+
+        if (link.parentElement.children[index] == index) {
+            navLink[index].classList.add("nav__link--active");
+            console.log(navLink[index]);
+        }
+
+        if (window.innerWidth > 768) {
+            navbar.classList.remove("nav--open-menu");
+            overlay.classList.remove("overlay");
+        } else {
+            toggleMenu();
+        };
+
+        // if (link.classList.contains("nav__link--active")) {
+        //     link.classList.remove("nav__link--active");
+        // } else {
+        //     link.classList.add("nav__link--active");
+        // }
+    });
+
 });
+
+//set scroll padding top dynamically
+document.documentElement.style.setProperty("--scroll-padding", (header.offsetHeight - 1) + "px");
+
+// for (var i = 0; i < navLink.length; i++) {
+//     navLink[i].addEventListener("click", function () {
+//         var current = document.getElementsByClassName(".nav__link");
+//         current[0].className = current[0].className.replace("nav__link--active", "");
+//         this.className += " nav__link--active";
+//     });
+// }
