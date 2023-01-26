241b3897f1d175b2badfa91e29ff1b57c74d5262
Roland Jethro Suyom <rolandjethrosuyom@yahoo.com>
2023-01-26 06:47:03 +0300

    fix testimonial cards responsiveness

--- app/js/script.js
+++ app/js/script.js
@@ -8,10 +8,10 @@ const sections = document.querySelectorAll("section");
 const fabButton = document.querySelector(".fab-button");
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2,
+    slidesPerView: 1,
     spaceBetween: 30,
-    slidesPerGroup: 2,
-    loop: true,
+    slidesPerGroup: 1,
+    //loop: true,
     loopFillGroupWithBlank: true,
     pagination: {
         el: ".swiper-pagination",
@@ -21,6 +21,24 @@ var swiper = new Swiper(".mySwiper", {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+        // // when window width is >= 320px
+        // 320: {
+        //     slidesPerView: 2,
+        //     spaceBetween: 20
+        // },
+        // // when window width is >= 480px
+        // 480: {
+        //     slidesPerView: 3,
+        //     spaceBetween: 30
+        // },
+        // when window width is >= 640px
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+            slidesPerGroup: 2,
+        }
+    }
 });
 
 /** auto hide toggle menu and overlay when the user resize the window */
